09a6f8e060a1463b580fa9d1bc4f4a1c18425993
komeno <tkomeno@student.42tokyo.jp>
2024-10-10 13:49:04 +0900

    chore: initial structure

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Komeno on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Komeno on 2024/10/10.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
